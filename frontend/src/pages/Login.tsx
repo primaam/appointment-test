20381898e73a67648d94e48452cb963fd6b643b4
primaam <pmaharyono@gmail.com>
2024-09-04 04:32:01 +0700

    update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login: React.FC = () => {
     return (
         <div>
-            <div>djshdjs</div>
+            <div>djsdfdfdfhdjs</div>
         </div>
     );
 };
